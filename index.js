bb8f4cacfc50e73bccd8be190215a12ac7157730
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-05-15 11:25:53 +0500

    Sigma5.2

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("Iltimos, ismingizni kiriting:");
+let spaceId = prompt("Iltimos, spaceId'ni kiriting:");
+let parol = prompt("Iltimos, spacePassword'ni kiriting:");
+
+let Info = {
+    ism: ism,
+    id: spaceId,
+    parol: parol
+};
+
+console.log(Info);
