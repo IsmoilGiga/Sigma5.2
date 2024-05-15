0b3f9fd0fc6ee12fb1c8ab45505ad8aae83bfbf4
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-05-15 11:27:04 +0500

    Sigma5.2

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let ism = prompt("Iltimos, ismingizni kiriting:");
 let spaceId = prompt("Iltimos, spaceId'ni kiriting:");
 let parol = prompt("Iltimos, spacePassword'ni kiriting:");
 
-let Info = {
+let info = {
     ism: ism,
     id: spaceId,
     parol: parol
 };
 
-console.log(Info);
+console.log(info);
